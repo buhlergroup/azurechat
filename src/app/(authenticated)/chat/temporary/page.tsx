a5bac9e070c94a5d8dcea9676c783bf4e8e689ab
Saddam Akhtar <saddam.akhtar@buhlergroup.com>
2025-09-08 09:48:16 +0530

    feat: Temporary chat
    (cherry picked from commit 626b64f46f5d5c8213e1fb047d3d27a451c4c8b7)

---
+++ src/app/(authenticated)/chat/temporary/page.tsx
@@ -0,0 +1,70 @@
+import { userHashedId } from "@/features/auth-page/helpers";
+import { ChatPage } from "@/features/chat-page/chat-page";
+import { FindAllChatDocuments } from "@/features/chat-page/chat-services/chat-document-service";
+import { FindAllChatMessagesForCurrentUser } from "@/features/chat-page/chat-services/chat-message-service";
+import {
+  CreateChatThread,
+  FindChatThreadForCurrentUser,
+  ResetChatThread,
+} from "@/features/chat-page/chat-services/chat-thread-service";
+import { FindAllExtensionForCurrentUserAndIds } from "@/features/extensions-page/extension-services/extension-service";
+import { AI_NAME, TEMPORARY_CHAT_NAME, TEMPORARY_CHAT_ROUTE } from "@/features/theme/theme-config";
+import { DisplayError } from "@/features/ui/error/display-error";
+import { redirect } from "next/navigation";
+
+export const metadata = {
+  title: AI_NAME,
+  description: AI_NAME,
+};
+
+interface HomeParams {
+  params: Promise<{
+    id: string;
+  }>;
+}
+
+export default async function Home(props: HomeParams) {
+  const userIdAsChatThreadId = await userHashedId();
+  const [chatResponse, chatThreadResponse, docsResponse] = await Promise.all([
+    FindAllChatMessagesForCurrentUser(userIdAsChatThreadId),
+    FindChatThreadForCurrentUser(userIdAsChatThreadId),
+    FindAllChatDocuments(userIdAsChatThreadId),
+  ]);
+
+  if (chatThreadResponse.status === "OK" && chatResponse.status === "OK" && chatResponse.response.length ) {
+    await ResetChatThread(userIdAsChatThreadId);
+    redirect(TEMPORARY_CHAT_ROUTE);
+  } else if (chatThreadResponse.status === "NOT_FOUND") {
+    await CreateChatThread({ id: userIdAsChatThreadId, name: TEMPORARY_CHAT_NAME, temporary: true });
+    redirect(TEMPORARY_CHAT_ROUTE);
+  }
+
+  if (docsResponse.status !== "OK") {
+    return <DisplayError errors={docsResponse.errors} />;
+  }
+
+  if (chatResponse.status !== "OK") {
+    return <DisplayError errors={chatResponse.errors} />;
+  }
+
+  if (chatThreadResponse.status !== "OK") {
+    return <DisplayError errors={chatThreadResponse.errors} />;
+  }
+
+  const extensionResponse = await FindAllExtensionForCurrentUserAndIds(
+    chatThreadResponse.response.extension
+  );
+
+  if (extensionResponse.status !== "OK") {
+    return <DisplayError errors={extensionResponse.errors} />;
+  }
+
+  return (
+    <ChatPage
+      messages={chatResponse.response}
+      chatThread={chatThreadResponse.response}
+      chatDocuments={docsResponse.response}
+      extensions={extensionResponse.response}
+    />
+  );
+}
